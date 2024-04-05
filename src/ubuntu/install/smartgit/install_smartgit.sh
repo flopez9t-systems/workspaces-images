7bb5518690e52e3ed03c44792ed70dfad92204a2
flopez9t-systems <150351022+flopez9t-systems@users.noreply.github.com>
2024-04-06 00:46:02 +0200

    Add files via upload

---
+++ src/ubuntu/install/smartgit/install_smartgit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ex
+
+# Install Smartgit
+wget -q https://www.syntevo.com/downloads/smartgit/smartgit-23_1_2.deb -O smartgit.deb
+apt-get update
+apt-get install -y ./smartgit.deb
+
+# Desktop icon
+rm smartgit.deb
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
