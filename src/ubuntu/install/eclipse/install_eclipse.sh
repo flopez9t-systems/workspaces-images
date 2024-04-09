69654196ce595c91975e9935fe2f550032c99497
flopez9t-systems <150351022+flopez9t-systems@users.noreply.github.com>
2024-04-09 17:57:07 +0200

    Update install_eclipse.sh

--- src/ubuntu/install/eclipse/install_eclipse.sh
+++ src/ubuntu/install/eclipse/install_eclipse.sh
@@ -5,3 +5,8 @@ ECLIPSE_VER_DATE="2024-03"
 cd /tmp
 wget -q -O eclipse.tar.gz "https://mirrors.xmission.com/eclipse/technology/epp/downloads/release/2024-03/R/eclipse-jee-2024-03-R-linux-gtk-x86_64.tar.gz"
 tar -xzf eclipse.tar.gz -C /opt
+
+ECLIPSE_ICON="/opt/eclipse/plugins/$(ls /opt/eclipse/plugins/ | grep -m 1 org.eclipse.platform_)/eclipse128.png"
+sed -i "s#eclipse128.png#${ECLIPSE_ICON}#" $INST_SCRIPTS/eclipse/eclipse.desktop
+cp $INST_SCRIPTS/eclipse/eclipse.desktop $HOME/Desktop/
+cp $INST_SCRIPTS/eclipse/eclipse.desktop /usr/share/applications/
