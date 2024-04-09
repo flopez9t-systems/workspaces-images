9cd531c31f0c40f9b1c5466413243b20c6897c66
flopez9t-systems <150351022+flopez9t-systems@users.noreply.github.com>
2024-04-09 17:34:51 +0200

    Update install_eclipse.sh

--- src/ubuntu/install/eclipse/install_eclipse.sh
+++ src/ubuntu/install/eclipse/install_eclipse.sh
@@ -4,4 +4,4 @@ ECLIPSE_VER_DATE="2024-03"
 
 cd /tmp
 wget -q -O eclipse.tar.gz "https://mirrors.xmission.com/eclipse/technology/epp/downloads/release/2024-03/R/eclipse-jee-2024-03-R-linux-gtk-x86_64.tar.gz"
-tar -xzf eclipse.tar.gz -C /tmp
+tar -xzf eclipse.tar.gz -C /opt
